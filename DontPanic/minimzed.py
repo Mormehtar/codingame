0d6a15189a9e32eebccd0c1aed3aca10a8e870c1
ilyavolkov <mormehtar@gmail.com>
2017-06-19 16:24:21 +0300

    don't panic minimzed to 220

--- DontPanic/minimzed.py
+++ DontPanic/minimzed.py
@@ -1,3 +1,3 @@
-_,_,_,z,x,_,_,e=input().split();d={z:int(x)}
+*_,z,x,_,_,e=input().split();d={z:int(x)}
 for i in range(int(e)):f,p=input().split();d[f]=int(p)
-while 1:v,b,n=input().split();b=int(b);t=d.get(v);print("WAIT"if t is None or t==b or not(t<b and n=='RIGHT'or t>b and n=='LEFT')else"BLOCK")
+while 1:v,b,n=input().split();b=int(b);t=d.get(v);print("WAIT"if(t is None)or(t==b)or(t>b if n!="LEFT"else t<b)else"BLOCK")
